0bd2f561b911e950308ba485fd1af0672fd09572
pedrocardoso5 <pedro.cardoso@ufrn.edu.br>
2020-05-04 09:20:07 -0300

    update

--- low_cost_drum/src/views/Home.vue
+++ low_cost_drum/src/views/Home.vue
@@ -7,52 +7,73 @@
   </div>
   <div class="rasta">
   </div>
-  <b-row align-v="center" align-h="end">
-    <b-col align-self="center" style="backgroung-color: #fff;">
-      <div class="b-111">
-        <b-col align-self="center">
-            <h2 style="color: #FF3B3F">Sobre a LAPERME</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tenetur voluptatum beatae, magnam unde saepe velit repudiandae dolores quis, cumque, assumenda ipsa fuga rerum aliquam hic? Aperiam consectetur nam nulla!
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. A quis quisquam sunt reprehenderit aperiam rerum ipsa aliquid corporis repellat molestiae autem suscipit temporibus, neque blanditiis placeat dolore explicabo. Et, provident!<br/>
-            </p>
-            <b-button variant= "secondary" to="/sobre">Saber Mais</b-button>
-        </b-col>
-      </div>
-    </b-col>
-    <b-col align-self="center">
-      <div class="b-12">
-        <b-img src="https://images2.imgbox.com/32/1e/yk8z7ZUH_o.jpg" fluid alt="Responsive image"></b-img>
-      </div>
-    </b-col>
-    <!-- <div class="w-100" style="height:'10px'"></div>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, ipsam facilis fuga molestiae quas deserunt consectetur nesciunt aperiam, aut itaque dolorem modi quaerat. Ab accusamus deleniti corrupti tenetur, officiis itaque!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptas provident repellendus totam corporis magni explicabo, ullam omnis doloribus velit, voluptatum necessitatibus! Amet hic excepturi magnam illum alias soluta eligendi!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis inventore velit alias, mollitia veritatis aliquam consequatur voluptate blanditiis dolorum, asperiores nam rem excepturi! Cumque natus laudantium beatae dolorem labore.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita quam adipisci corporis praesentium qui pariatur eos non vel quas quis, eum molestias repellendus placeat officia laboriosam hic earum doloribus!
-    </p> -->
-  </b-row>
-    <b-row align-v="center" align-h="end" class="b-11">
-          <b-col>
-            <b-img src="https://i0.wp.com/eletronworld.com.br/wp-content/uploads/2016/03/Circuito-1.png?fit=616%2C454" fluid alt="Responsive image"></b-img>
+  <div class="conteudo">
+    <b-row align-v="center" align-h="end">
+      <b-col align-self="center" style="backgroung-color: #fff;">
+        <div class="b-111">
+          <b-col align-self="center">
+              <h2 style="color: #FF3B3F">Sobre a LAPERME</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam tenetur voluptatum beatae, magnam unde saepe velit repudiandae dolores quis, cumque, assumenda ipsa fuga rerum aliquam hic? Aperiam consectetur nam nulla!
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A quis quisquam sunt reprehenderit aperiam rerum ipsa aliquid corporis repellat molestiae autem suscipit temporibus, neque blanditiis placeat dolore explicabo. Et, provident!<br/>
+              </p>
+              <b-button variant= "secondary" to="/sobre">Saber Mais</b-button>
           </b-col>
-          <b-col>
+        </div>
+      </b-col>
+      <b-col align-self="center" style="padding-right: 0px; padding-left: 0px;">
+          <b-img src="https://images2.imgbox.com/32/1e/yk8z7ZUH_o.jpg" fluid alt="Responsive image"></b-img>
+      </b-col>
+      <!-- <div class="w-100" style="height:'10px'"></div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, ipsam facilis fuga molestiae quas deserunt consectetur nesciunt aperiam, aut itaque dolorem modi quaerat. Ab accusamus deleniti corrupti tenetur, officiis itaque!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptas provident repellendus totam corporis magni explicabo, ullam omnis doloribus velit, voluptatum necessitatibus! Amet hic excepturi magnam illum alias soluta eligendi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis inventore velit alias, mollitia veritatis aliquam consequatur voluptate blanditiis dolorum, asperiores nam rem excepturi! Cumque natus laudantium beatae dolorem labore.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita quam adipisci corporis praesentium qui pariatur eos non vel quas quis, eum molestias repellendus placeat officia laboriosam hic earum doloribus!
+      </p> -->
+    </b-row>
+    <b-row align-v="center" align-h="end" style="backgroung-color: white;">
+      <b-col align-self="center">
+        <div class="b-111">
+          <b-col align-self="center" style="padding-right: 0px; padding-left: 0px;">
+            <b-img src="https://images.unsplash.com/photo-1553063085-dbbf64d936ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80" fluid alt="Responsive image"></b-img>
+          </b-col>
+        </div>
+      </b-col>
+      <b-col align-self="center" style="padding-right: 0px; padding-left: 0px;">
+          <div style="padding-right: 15px; padding-left: 0px;">
             <h2 style="color: #FF3B3F">Nossos Projetos</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis corrupti quia enim aliquam quisquam corporis incidunt? Iste tenetur enim culpa incidunt aliquid fugit eius iure velit, soluta necessitatibus maiores corrupti.<br/>
-            <b-button variant= "secondary" to="/Tutorial">Saber Mais</b-button>
-            </b-col>
-          <div class="w-100"></div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis corrupti quia enim aliquam quisquam corporis incidunt? Iste tenetur enim culpa incidunt aliquid fugit eius iure velit, soluta necessitatibus maiores corrupti.<br/>
+              <b-button variant= "secondary" to="/Tutorial">Saber Mais</b-button>
+          </div>
+      </b-col>
+      <!-- <div class="w-100" style="height:'10px'"></div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, ipsam facilis fuga molestiae quas deserunt consectetur nesciunt aperiam, aut itaque dolorem modi quaerat. Ab accusamus deleniti corrupti tenetur, officiis itaque!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptas provident repellendus totam corporis magni explicabo, ullam omnis doloribus velit, voluptatum necessitatibus! Amet hic excepturi magnam illum alias soluta eligendi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis inventore velit alias, mollitia veritatis aliquam consequatur voluptate blanditiis dolorum, asperiores nam rem excepturi! Cumque natus laudantium beatae dolorem labore.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita quam adipisci corporis praesentium qui pariatur eos non vel quas quis, eum molestias repellendus placeat officia laboriosam hic earum doloribus!
+      </p> -->
     </b-row>
     <b-row align-v="center" align-h="end">
-          <b-col>
-            <h2 style="color: #FF3B3F">Fale Conosco</h2>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis fugit eum aliquid rem! Fugiat rerum cum similique nam labore qui illo, dolor ex iure voluptatibus fugit alias quisquam expedita distinctio.<br/>
-          <b-button variant= "secondary" to="/contato">Saber Mais</b-button>
+      <b-col align-self="center" style="backgroung-color: #fff;">
+        <div class="b-111">
+          <b-col align-self="center">
+              <h2 style="color: #FF3B3F">Fale Conosco</h2>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis fugit eum aliquid rem! Fugiat rerum cum similique nam labore qui illo, dolor ex iure voluptatibus fugit alias quisquam expedita distinctio.<br/>
+            <b-button variant= "secondary" to="/contato">Saber Mais</b-button>
           </b-col>
-          <b-col>
-            <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"></b-img>
-          </b-col>
-        </b-row>
+        </div>
+      </b-col>
+      <b-col align-self="center" style="padding-right: 0px; padding-left: 0px;">
+          <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"></b-img>
+      </b-col>
+      <!-- <div class="w-100" style="height:'10px'"></div>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, ipsam facilis fuga molestiae quas deserunt consectetur nesciunt aperiam, aut itaque dolorem modi quaerat. Ab accusamus deleniti corrupti tenetur, officiis itaque!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae voluptas provident repellendus totam corporis magni explicabo, ullam omnis doloribus velit, voluptatum necessitatibus! Amet hic excepturi magnam illum alias soluta eligendi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quis inventore velit alias, mollitia veritatis aliquam consequatur voluptate blanditiis dolorum, asperiores nam rem excepturi! Cumque natus laudantium beatae dolorem labore.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil expedita quam adipisci corporis praesentium qui pariatur eos non vel quas quis, eum molestias repellendus placeat officia laboriosam hic earum doloribus!
+      </p> -->
+    </b-row>
+  </div>
       <div class="rasta">
         </div>
     <div class="Sapato" style="backgroung-color: #fff;">
@@ -63,9 +84,23 @@
 </div>
 </template>
 <style>
+.row{
+  margin-right: 0px !important;
+}
+.conteudo{
+  max-width: 100%;
+}
 .bv-example-row{
   height: 700px;
   margin-top: auto;
+}
+.bv-example-row2{
+  height: 700px;
+  max-width: 100% !important;
+  margin-top: auto;
+}
+.align-self-center{
+  padding-right: 0px !important;
 }
 .rasta{
   width: 100%;
